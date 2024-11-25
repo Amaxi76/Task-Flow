2daba04d99593ccc131c1eb4093aae1f767fe60d
Maxime Lemoine <maxime.lemoine1@etu.univ-lehavre.fr>
2024-11-25 20:21:05 +0100

    correction envoit données

--- app/Controllers/PersonneControleur.php
+++ app/Controllers/PersonneControleur.php
@@ -7,18 +7,10 @@ class PersonneControleur extends BaseController
 	public function index() 
 	{ 
 		$personneModel = new PersonneModele();
-		// Récupérer les paramètres de la requête avec des valeurs par défaut 
-		$email = $this->request->getVar('email') ?? 'defaut';
-		$nom   = $this->request->getVar('nom') ?? 'defaut';  // Par défaut tri par titre 
-		$mdp   = $this->request->getVar('mdp') ?? 'defaut'; // Par défaut croissant 
 
-
-		$data['email'] = $email;
-		$data['nom'] = $nom;
-		$data['mdp'] = $mdp;
-	
+		$personnes = $personneModel->orderBy('nom', 'asc')->findAll();
 
 		// Charger la vue 
-		return view('personneVue', $data); 
+		return view('personneVue', $personnes); 
 	} 
 } 
