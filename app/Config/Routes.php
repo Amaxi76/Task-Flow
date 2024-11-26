0e22fbd2ce7dc031bb64235e373283a6e8cd30b3
Maxime Lemoine <maxime.lemoine1@etu.univ-lehavre.fr>
2024-11-26 09:38:58 +0100

    nettoyage simple

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,7 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-$routes->get('/','Home::index');
+$routes->get('/','PersonneControleur::index');
 $routes->get('/test','PersonneControleur::index');
 
 
