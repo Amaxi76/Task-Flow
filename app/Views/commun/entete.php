392a2233a3786f03c43936f5b0da2f65c1185a81
Amaxi76 <lesterlinpro@gmail.com>
2024-11-27 12:51:00 +0100

    Finalisation du header de la pag des tâches

--- app/Views/commun/entete.php
+++ app/Views/commun/entete.php
@@ -13,35 +13,3 @@
 	</head>
 
 	<body>
-		<header id="up-link">
-			<!--<img src="/assets/images/taskflow.png" alt="Logo de TaskFlow">-->
-			<h1><?php echo $titre ?></h1>
-
-			<style>
-				#menu {
-					display: flex;
-					list-style-type: none;
-					justify-content: space-between;
-				}
-
-				.tache {
-					border: 1px solid black;
-					padding: 10px;
-					margin: 10px;
-				}
-			</style>
-			
-			<ul id="menu">
-				<li><a href="#">Date</a></li>
-				<li><a href="#">Priorité</a></li>
-				<li><a href="#">Détails</a></li>
-				<li>|</li>
-				<li><a href="#">Statuts</a></li>
-				<li><a href="#">Retard</a></li>
-				<li><a href="#">Priorité</a></li>
-				<li>|</li>
-				<li><a href="#">Recherche</a></li>
-			</ul>
-
-			<button>Ajouter une tâche</button>
-		</header>
