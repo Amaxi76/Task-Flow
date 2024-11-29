b917ca5a999452165db073309dd2ee98553d2ee5
Maxime Lemoine <maxime.lemoine1@etu.univ-lehavre.fr>
2024-11-29 15:22:01 +0100

    suppression auto des tables lors des suppressions + ajout de commandes sql pour utilisation en développement

--- sql/reintialisation.sql
+++ sql/reintialisation.sql
@@ -1,7 +1,7 @@
 -- Seul fichier nécessaire pour réinitialiser la base de données
 -- Et de générer toutes les tables / vues et données nécessaires
 
-\i nettoyer.sql
+\i suppression/nettoyer.sql
 \i creation/creation_schema.sql
 \i creation/creation_tables.sql
 \i creation/creation_vues.sql
