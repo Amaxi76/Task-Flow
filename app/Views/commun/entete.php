c1449b4b0dc861da99145d29ce6251df7a9251b2
Amaxi76 <lesterlinpro@gmail.com>
2024-12-01 18:00:06 +0100

    Correction de redirection sur le header et corrections graphiques

--- app/Views/commun/entete.php
+++ app/Views/commun/entete.php
@@ -13,13 +13,27 @@
 
 <header>
 	<div class="conteneur-entete">
-		<img src="<?= base_url('assets/images/Task-Flow-Horizontal.svg') ?>" alt="Logo de TaskFlow">
+		<a href="<?= base_url('/') ?>">
+			<img src="<?= base_url('assets/images/Task-Flow-Horizontal.svg') ?>" alt="Logo de TaskFlow">
+		</a>
 		
-		<div class="button main-button">
-			<svg viewBox="0 0 43 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<path d="M27.5294 24.0714V30.9524C27.5294 33.1878 25.7298 35 23.5098 35H5.0196C2.79964 35 1 33.1878 1 30.9524V5.04762C1 2.81218 2.79964 1 5.01961 1H23.5098C25.7298 1 27.5294 2.81218 27.5294 5.04762V11.9286M11.8529 18H42M42 18L35.9704 11.9286M42 18L35.9704 24.0715" />
-			</svg>
-			<button>Déconnexion</button>
+		<div class="conteneur-menu-boutons">
+			<a href="<?= base_url('/profil')?>">
+				<div class="button secondary-button">
+					<button>Profil</button>
+				</div>
+			</a>	
+		
+			<a href="<?= base_url('/deconnexion')?>">
+				<div class="button main-button">
+					<svg viewBox="0 0 43 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M27.5294 24.0714V30.9524C27.5294 33.1878 25.7298 35 23.5098 35H5.0196C2.79964 35 1 33.1878 1 30.9524V5.04762C1 2.81218 2.79964 1 5.01961 1H23.5098C25.7298 1 27.5294 2.81218 27.5294 5.04762V11.9286M11.8529 18H42M42 18L35.9704 11.9286M42 18L35.9704 24.0715" />
+					</svg>
+					<button>Déconnexion</button>
+				</div>
+			</a>			
 		</div>
+
+		
 	</div>
 </header>
